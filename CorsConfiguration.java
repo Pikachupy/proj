203380acdb92a7f717cdfc45be876e20c57524d7
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2022-03-23 22:35:54 +0100

    Create CorsConfiguration.java

---
+++ CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.wspolbiegi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*"); // powinno zadziałać, ale no idea :>
+    }
+}
